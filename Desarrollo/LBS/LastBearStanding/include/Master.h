e728e4343dffb180c01a95847165713ab357fd4d
tzrober <tzrober91@gmail.com>
2017-04-20 16:25:31 +0200

    Mapa 3 modificado sin teleports

--- Desarrollo/LBS/LastBearStanding/include/Master.h
+++ Desarrollo/LBS/LastBearStanding/include/Master.h
@@ -30,10 +30,10 @@ class Master{
         const Num2Map maps[7] = {
               {  1         , L"media/Maps/MapFinding1.tmx" }
             , {  2         , L"media/Maps/MapFinding2.tmx" }
-            , {  4         , L"media/Maps/MapFinding4.tmx" }
-            , {  5         , L"media/Maps/MapFinding5.tmx" }
-            , {  6         , L"media/Maps/MapFinding6.tmx" }            
-            , {  3         , L"media/Maps/MapFinding1.tmx" }
+            , {  4         , L"media/Maps/MapFinding3.tmx" }
+            , {  5         , L"media/Maps/MapFinding4.tmx" }
+            , {  6         , L"media/Maps/MapFinding5.tmx" }            
+            , {  3         , L"media/Maps/MapFinding6.tmx" }
             , {  0         , L"0"                          }
         };
 };

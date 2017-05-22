5c2157f86b43039244eeb7eb044577de3f0ea603
MiguelPaniagua <Miguel_lol_sennin@hotmail.com>
2017-05-22 14:36:36 +0200

    Num partidas ajustado

--- Desarrollo/LBS+BearEngine/LastBearStanding/src/Master.cpp
+++ Desarrollo/LBS+BearEngine/LastBearStanding/src/Master.cpp
@@ -90,7 +90,7 @@ void Master::UpdateGame(){
         }
         else if(SDL_GetTicks()-timeFinPartida>FINPARTIDA){
             puntuaciones[World::Inst()->getGanador()]++;
-            if(puntuaciones[World::Inst()->getGanador()]==1){
+            if(puntuaciones[World::Inst()->getGanador()]==6){
                 estado++;
                 finPartidaObject->setWinner(World::Inst()->getGanador());
 
